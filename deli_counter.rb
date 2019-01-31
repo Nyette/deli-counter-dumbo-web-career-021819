78941fc7dd52ee69c4bcdf8adc4fd37358b44223
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 09:22:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,15 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.size > 0
-    puts     
+    text = "The line is currently: "
+    katz_deli.each_with_index do | person, index |
+      text += "#{index + 1}. #{person}"
+    end
+    return text
   else
-    puts "The line is currently empty."
+    return "The line is currently empty."
+  end
 end
+
+
 
