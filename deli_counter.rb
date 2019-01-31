1cf97e2328a9fa9ba36b221eb8d2c2fdf01d67f2
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 09:28:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,8 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.size > 0
-    text = "The line is currently: "
     katz_deli.each_with_index do | person, index |
-      text += "#{index + 1}. #{person} "
+      text = "The line is currently: #{index + 1}. #{person}"
     end
     puts text
   else
