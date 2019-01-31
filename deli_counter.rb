8798a9518589be99e3866c85eb901418c64df263
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 09:33:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size > 0
     katz_deli.each_with_index do |person, index|
-      puts "#{index + 1}. #{person}"
+      puts " #{index + 1}. #{person}"
     end
   else
     puts "The line is currently empty."
