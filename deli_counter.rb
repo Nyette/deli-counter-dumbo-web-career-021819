169aef0f8df40bb80d0d5f51c48b53b96cca193b
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 09:47:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,13 @@ def take_a_number(katz_deli, new_person)
   puts "Welcome, #{new_person}. You are number #{katz_deli.size} in line."
 end
 
-def 
+def now_serving(katz_deli)
+  if katz_deli.size > 0
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
+
 
